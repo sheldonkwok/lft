0cfc087d689e0a86bd3057e059aec80b296c01a3
Sheldon Kwok <me@sheldonk.com>
2026-04-13 15:15:07 -0700

    feat: Add some leg exercises

--- src/exercises.ts
+++ src/exercises.ts
@@ -9,4 +9,6 @@ export type Exercise = {
 export const EXERCISES: Exercise[] = [
 	{ name: "Bench", muscleGroup: "chest", equipment: "barbell" },
 	{ name: "Incline Bench", muscleGroup: "chest", equipment: "barbell" },
+	{ name: "Squat Plyo", muscleGroup: "legs", equipment: "barbell" },
+	{ name: "Reverse Lunge", muscleGroup: "legs", equipment: "barbell" },
 ];
